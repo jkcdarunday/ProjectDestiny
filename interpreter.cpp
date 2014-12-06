cf20a98667642d139aec939054c874f793dfa543
Jan Keith Darunday <jkcdarunday@gmail.com>
2014-12-07 01:13:57 +0800

    Cleaned some of the debug lines

--- interpreter.cpp
+++ interpreter.cpp
@@ -125,10 +125,8 @@ void Interpreter::execute()
 {
     int lastN = 0;
     do{
-        //        qDebug() << "Parsing:" << this->lexemes->at(lastN).token;
         //i has a var
         if(syntaxCheck(lastN,"iv\n")){
-            //            qDebug() << "I has a var";
             if(!this->symbols->contains(lexemes->at(lastN+1).token))
                 this->symbols->insert(lexemes->at(lastN+1).token, new VariableData('x',""));
             else
@@ -141,7 +139,6 @@ void Interpreter::execute()
                 qDebug() << "Syntax Error : Declaring already-existing variable to string :(";
         }
         else if(syntaxCheck(lastN,"v:t\n")){
-            //            qDebug() << "var r string";
             if(this->symbols->contains(lexemes->at(lastN).token)){
                 this->symbols->value(lexemes->at(lastN).token)->value=lexemes->at(lastN+2).token;
                 this->symbols->value(lexemes->at(lastN).token)->type=lexemes->at(lastN+2).type;
@@ -150,7 +147,6 @@ void Interpreter::execute()
         }
 
         else if(syntaxCheck(lastN, ".v\n")){
-            //qDebug() << "Immaprintthisvar";
             if(this->symbols->contains(this->lexemes->at(lastN+1).token) && QString("f01\"").contains(this->symbols->value(this->lexemes->at(lastN+1).token)->type))
                 emit output(this->symbols->value(this->lexemes->at(lastN+1).token)->value);
             else
